9fa595ee32aa808752d0c6e6a45ffd6baa8cff3b
Adrian Soto <sotoadrian4@gmail.com>
2018-10-30 20:24:31 -0600

    Tablas
    Se finaliza con la segunda parte de la demo, la cual consiste en mostrar las tablas de multiplicar seleccionadas

---
+++ Mvvm_Demo/Mvvm_Demo_Model/Cls_Tablas_Model.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+
+namespace Mvvm_Demo_Model
+{
+    public class Tabla
+    {
+        int _iTabla;
+        String _sDisplay;
+
+        public int ITabla
+        {
+            get
+            {
+                return _iTabla;
+            }
+
+            set
+            {
+                _iTabla = value;
+            }
+        }
+
+        public string SDisplay
+        {
+            get
+            {
+                return _sDisplay;
+            }
+
+            set
+            {
+                _sDisplay = value;
+            }
+        }
+    }
+
+    public class Cls_Tablas_Model
+    {
+        String _bNumeroInicio;
+        String _bNumeroFin;
+        Tabla _tTabla;
+        ObservableCollection<Tabla> _ocTablas;
+        ObservableCollection<string> _ocTablaFinal;
+
+        public string BNumeroInicio
+        {
+            get
+            {
+                return _bNumeroInicio;
+            }
+
+            set
+            {
+                _bNumeroInicio = value;
+            }
+        }
+
+        public string BNumeroFin
+        {
+            get
+            {
+                return _bNumeroFin;
+            }
+
+            set
+            {
+                _bNumeroFin = value;
+            }
+        }
+
+        public ObservableCollection<Tabla> OcTablas
+        {
+            get
+            {
+                return _ocTablas;
+            }
+
+            set
+            {
+                _ocTablas = value;
+            }
+        }
+
+        public ObservableCollection<string> OcTablaFinal
+        {
+            get
+            {
+                return _ocTablaFinal;
+            }
+
+            set
+            {
+                _ocTablaFinal = value;
+            }
+        }
+
+        public Tabla TTabla
+        {
+            get
+            {
+                return _tTabla;
+            }
+
+            set
+            {
+                _tTabla = value;
+            }
+        }
+    }
+}
---
+++ Mvvm_Demo/Mvvm_Demo_ViewModel/Cls_Tablas_ViewModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mvvm_Demo_Model;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Mvvm_Demo_ViewModel
+{
+    public class Cls_Tablas_ViewModel : ObservableObject
+    {
+        private Cls_Tablas_Model ecTabla;
+
+        public Cls_Tablas_ViewModel()
+        {
+            this.ecTabla = new Cls_Tablas_Model();
+            BNumeroInicio = "0";
+            BNumeroFin = "0";
+        }
+
+        public string BNumeroInicio
+        {
+            get
+            {
+                return ecTabla.BNumeroInicio;
+            }
+
+            set
+            {
+                ecTabla.BNumeroInicio = value;
+                RaisePropertyChangedEvent("BNumeroInicio");
+            }
+        }
+
+        public string BNumeroFin
+        {
+            get
+            {
+                return ecTabla.BNumeroFin;
+            }
+
+            set
+            {
+                ecTabla.BNumeroFin = value;
+                RaisePropertyChangedEvent("BNumeroFin");
+            }
+        }
+
+        public ObservableCollection<Tabla> OcTablas
+        {
+            get
+            {
+                return ecTabla.OcTablas;
+            }
+
+            set
+            {
+                ecTabla.OcTablas = value;
+                RaisePropertyChangedEvent("OcTablas");
+            }
+        }
+
+        public ObservableCollection<string> OcTablaFinal
+        {
+            get
+            {
+                return ecTabla.OcTablaFinal;
+            }
+
+            set
+            {
+                ecTabla.OcTablaFinal = value;
+                RaisePropertyChangedEvent("OcTablaFinal");
+            }
+        }
+
+        public Tabla TTabla
+        {
+            get
+            {
+                return ecTabla.TTabla;
+            }
+
+            set
+            {
+                ecTabla.TTabla = value;
+                CalcularTabla();
+                RaisePropertyChangedEvent("TTabla");
+            }
+        }
+
+        public ICommand CalcularListaTablasCommand
+        {
+            get { return new DelegateCommand(CalcularListaTablas); }
+        }
+
+        public void CalcularListaTablas()
+        {
+            OcTablas = new ObservableCollection<Tabla>();
+            Byte bInicio = Convert.ToByte(BNumeroInicio);
+            Byte bFin = Convert.ToByte(BNumeroFin);
+
+            for (Byte i = bInicio; i <= bFin; i++)
+            {
+                OcTablas.Add(new Tabla() { ITabla = i, SDisplay = string.Format("Tabla de multiplicar del {0}", i) });
+            }
+        }
+
+        public void CalcularTabla()
+        {
+            OcTablaFinal = new ObservableCollection<String>();
+
+            if (TTabla != null)
+            {
+                for (int i = 0; i <= 12; i++)
+                {
+                    OcTablaFinal.Add(TTabla.ITabla.ToString() + " x " + i.ToString() + " = " + (TTabla.ITabla * i).ToString());
+                }
+            }
+        }
+
+        public ICommand LimpiartablasCommand
+        {
+            get { return new DelegateCommand(Limpiartablas); }
+        }
+
+        public void Limpiartablas()
+        {
+            BNumeroInicio = "0";
+            BNumeroFin = "0";
+            OcTablas = null;
+            OcTablaFinal = null;
+            TTabla = null;
+        }
+
+    }
+}
